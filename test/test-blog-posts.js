015746adf5920afe8f55be2b457e05959bf782a8
tanyagriego <tanya.griego@gmail.com>
2019-01-07 17:24:33 -0800

    added delete endpoint

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -34,9 +34,40 @@ function seedBlogPostData () {
                 firstName: faker.name.firstName(),
                 lastName: faker.name.lastName()
             },
-            title: faker.lorem.sentence();
-            content: faker.lorem.text();
+            title: faker.lorem.sentence(),
+            content: faker.lorem.text()
         });
     }
     return BlogPost.insertMany(seedData);
 }
+
+//describe, it, and beforeEach are part of Mocha 
+describe('blog post API resource', function () {
+    before (function () {
+        return runServer(TEST_DATABASE_URL);
+    });
+
+});
+
+//last is delete
+
+describe('DELETE endpoint', function () {
+    it ('should delete a post by id', function (){
+
+        let post;
+
+        return BlogPost
+        .findOne()
+        .then(_post => {
+            post = _post;
+            return chai.request(app).delete(`/posts/${post.id}`);
+        })
+        .then(res => {
+            res.should.have.status(204);
+            return BlogPost.findById(post.id);
+        })
+        .then(_post => {
+            should.not.exist(_post);
+        });
+    });
+});
